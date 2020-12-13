629745a4bc27513bbc9632a6a6a85ac6110e4962
Bob Dyksen <bob@dyksen.associates>
2020-12-13 13:10:31 -0600

    changed main message on feature

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Bye there!</p>
+        <h1 className="App-title">Welcome to React</h1>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -17,6 +17,9 @@ function App() {
           Learn React
         </a>
       </header>
+      <p className="App-intro">
+        This was changed on the feature branch.
+      </p>
     </div>
   );
 }
